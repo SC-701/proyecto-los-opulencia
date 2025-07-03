3d0a1c7b4dc4e2cd1a933f3bbc70d39304cd24b2
Laboratorios <Laboratorios@R20>
2025-07-02 19:52:02 -0600

    interfaces y modelo de consultorio agregados en abstracciones

---
+++ APIClinicaDental/API/Abstracciones/Interface/DA/IConsultorioDA.cs
@@ -0,0 +1,22 @@
+﻿using Abstracciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Interface.DA
+{
+    public interface IConsultorioDA
+    {
+        public Task<IEnumerable<ConsultorioResponse>> ObtenerConsultorio();
+
+        public Task<ConsultorioResponse> ObtenerConsultorio(Guid id);
+
+        public Task<Guid> AgregarConsultorio(ConsultorioRequest request);
+
+        public Task<Guid> EditarConsultorio(Guid id, ConsultorioRequest request);
+
+        public Task<Guid> EliminarConsultorio(Guid idConsultorio);
+    }
+}
---
+++ APIClinicaDental/API/Abstracciones/Models/Consultorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Models
+{
+    public class Consultorio
+    {
+        public int Nombre { get; set; }
+        public string Ubicacion { get; set; }
+    }
+
+    public class  ConsultorioRequest : Consultorio
+    {
+        public int idEstado { get; set; }
+    }
+
+    public class ConsultorioResponse : Consultorio
+    {
+        public Guid idConsultorio { get; set; }
+        public string? Estado { get; set; }
+    }
+}
