2fef91623ac1b37004f0102d58437e3417de072d
Emanuel <emanurlperez85@gmail.com>
2025-08-13 16:19:17 -0600

    cambios services

--- WebApplicationClinicaDental/src/pages/Servicios.jsx
+++ WebApplicationClinicaDental/src/pages/Servicios.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import Agregar from '../components/Botones/Agregar'
-import ModalAgregar from '../components/Modals/ModalAgregarCitas/ModalAgregar'
 import Header from '../components/Header/Header'
 import { motion } from 'framer-motion'
 import { CirclePlus } from 'lucide-react'
 import Tabla from '../components/Tabla/Tabla'
-import { useServicios } from '../hooks/useServicio'
+import { useServicioEstadoEditar, useServicioObtenerActivos, useServicioObtenerInactivos, useServicioObtenerSumaCosto, useServicioObtenerTotales, useServicios } from '../hooks/useServicio'
 import { columnsServicios } from '../assets/constants/TablaDashboard'
+import ModalAgregarServicio from '../components/Modals/ModalAgregarServicios/ModalAgregarServicio'
+import DashBoardCard from '../components/Cards/DashBoardCard'
+import { DataCardServicios } from '../utils/utilsServicios'
 
 const Servicios = () => {
     const { servicios, cargarServicios } = useServicios();
-    const cargarEstado = async () => {
+    const { editarEstado } = useServicioEstadoEditar();
+    const  {ServiciosActivos , obtenerServiciosActivosAsync} = useServicioObtenerActivos();
+    const {ServiciosTotales , obtenerServiciosTotalesAsync} = useServicioObtenerTotales();
+    const {ServiciosInactivos , obtenerServiciosInactivosAsync} = useServicioObtenerInactivos();
+    const {ServiciosSumaCosto , obtenerServiciosSumaCostoAsync} = useServicioObtenerSumaCosto();
+    const cargarEstado = async (id, estado) => {
+        await editarEstado(id, estado);
         await cargarServicios();
+        await obtenerServiciosActivosAsync();
+        await obtenerServiciosTotalesAsync();
+        await obtenerServiciosInactivosAsync();
+        await obtenerServiciosSumaCostoAsync();
     };
+
+
+
+
+    const DataCardServ = DataCardServicios ({
+        ServiciosTotales,
+        ServiciosActivos,
+        ServiciosInactivos,
+        ServiciosSumaCosto
+    })
+
+    const handleSuccess = async () => {
+        await Promise.all([
+            cargarServicios(),
+            obtenerServiciosActivosAsync(),
+            obtenerServiciosTotalesAsync(),
+            obtenerServiciosInactivosAsync(),
+            obtenerServiciosSumaCostoAsync()
+        ])
+    }
 
     return (
         <div className='flex-1 overflow-auto relative z-10'>
@@ -24,6 +56,22 @@ const Servicios = () => {
                     </h1>
                 </div>
                 <motion.div
+                    className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    {DataCardServ.map((card, index) => (
+                        <DashBoardCard
+                            key={index}
+                            nombre={card.nombre}
+                            icon={card.icon}
+                            valor={card.valor}
+                            color={card.color}
+                        />
+                    ))}
+                </motion.div>
+                <motion.div
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1 }}
@@ -32,13 +80,13 @@ const Servicios = () => {
                         <div className='bg-white bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl p-6 mt-6'>
                             <div className='flex justify-between items-center mb-4'>
                                 <h1 className='text-2xl font-bold py-4'>Ofrecimiento de Servicios</h1>
-                                <Agregar icon={<CirclePlus />} title="Agregar Servicio" modalName="my_modal_6" />
+                                <Agregar icon={<CirclePlus />} title="Agregar Servicio" modalName="my_modal_servicio" />
                             </div>
                             <Tabla data={servicios} columns={columnsServicios(cargarEstado)} />
                         </div>
                     </div>
                 </motion.div>
-                <ModalAgregar idModal="my_modal_6" />
+                <ModalAgregarServicio idModal="my_modal_servicio" onSuccess={handleSuccess} />
 
             </main>
 
---
+++ WebApplicationClinicaDental/src/services/Estados.js
@@ -0,0 +1,2 @@
+import axios from './AxiosInstance.service';
+const estados = 'estados'
